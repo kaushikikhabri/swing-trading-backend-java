874280db818414a29856eb8c863f29a27a484485
kaushiki_khabri <kaushiki.khabri@gmail.com>
2024-10-28 10:29:51 +0530

    First connection

---
+++ src/main/java/com/example/swing_trading_backend/SwingTradingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.swing_trading_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingTradingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwingTradingBackendApplication.class, args);
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/com/example/swing_trading_backend/SwingTradingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swing_trading_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwingTradingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
