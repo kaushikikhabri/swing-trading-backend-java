fefb9d095ea08bf38adc3663b9a05a7135cfa702
Lisa Susheel <lisasusheel12@gmail.com>
2024-11-28 13:19:14 +0530

    Dummy API for createWatchlist

---
+++ src/main/java/com/example/swing_trading_backend/controller/WatchlistController.java
@@ -0,0 +1,2 @@
+package com.example.swing_trading_backend.controller;public class WatchlistController {
+}
---
+++ src/main/java/com/example/swing_trading_backend/dto/WatchlistRequest.java
@@ -0,0 +1,2 @@
+package com.example.swing_trading_backend.dto;public class WatchlistRequest {
+}
